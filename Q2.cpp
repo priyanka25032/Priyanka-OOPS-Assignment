40aed06a77d5747aaa26546a13df63f68fd6c365
priyanka25032 <ppriyyanka.2505@gmail.com>
2026-09-05 13:44:54 +0530

    Finding area of rectangle usingparameterized constructor

---
+++ Q2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Rectangle {
+    float lenght , breadth;
+    public:
+    Rectangle (float l, float b) {
+        lenght=l;
+        breadth=b;
+    }
+    void display() {
+        cout<<"Area:"<<lenght*breadth;
+    }
+};
+int main() {
+    Rectangle s(12,12);
+    s.display();
+    return 0;
+}
